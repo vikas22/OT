5f2d67f7021cbe021bc71eb6d888907861c0d786
Vikas <vck173@gmail.com>
2017-03-05 21:55:09 +0530

    sample upload to s3 storage

--- OpenTable.rb
+++ OpenTable.rb
@@ -1,16 +1,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'aws-sdk'
+
+
+REGION = 'ap-southeast-1'
+ACCESS_KEY = 'YOUR_KEY'
+ACCESS_SEC = 'YOUR_SECRET'
+BUCKET_FOLDER = 'images'
+
+Aws.config.update({
+  region: REGION,
+  credentials: Aws::Credentials.new(ACCESS_KEY, ACCESS_SEC)
+})
+
+S3 = Aws::S3::Resource.new(region: REGION)
 
 get '/' do
   haml :upload
 end
 
 post "/" do 
-  File.open('uploads/' + params[:myImage][:filename], "w") do |f|
-    f.write(params['myImage'][:tempfile].read)
-  end
-  return "The file was successfully uploaded!"
+	puts params
+	obj = S3.bucket('otimagestest').object(BUCKET_FOLDER + "/" + params['myImage'][:filename])
+	obj.upload_file(params['myImage'][:tempfile])
 end
 
 
