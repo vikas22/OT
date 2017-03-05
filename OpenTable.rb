99cd86c5b5c70d497e473cbc79ac5ef1ffca99d3
Vikas <vck173@gmail.com>
2017-03-05 21:04:56 +0530

    haml view for sample upload

--- OpenTable.rb
+++ OpenTable.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  haml :upload
 end
+
+post "/" do 
+  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  return "The file was successfully uploaded!"
+end
+
+
