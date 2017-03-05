85af74df7a5068a12053a3e0befb54514acecb0a
Vikas <vck173@gmail.com>
2017-03-05 23:59:33 +0530

    group by uploaded date

--- OpenTable.rb
+++ OpenTable.rb
@@ -24,14 +24,17 @@ end
 
 post "/" do 
 	puts params
+
 	obj = S3.bucket(BUCKET).object(BUCKET_FOLDER + "/" + params['myImage'][:filename])
 	obj.upload_file(params['myImage'][:tempfile])
 	redirect "images"
 end
 
 get '/images' do 
-	images = S3.bucket(BUCKET).objects.select{|x| !is_image?(x.key)}
-	haml :images, :locals => {:bucket => images, :url => S3_URL }
+
+	imagesArr= (S3.bucket(BUCKET).objects.select{|x| !is_image?(x.key)})
+	imagesHash =  imagesArr.group_by{|x| x.last_modified.strftime("%m/%d/%Y")}
+	haml :images, :locals => {:images => imagesHash, :url => S3_URL }
 end
 
 def is_image?(key)
