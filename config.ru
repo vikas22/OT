88d9d6e0c7c4b5ce756de3216ef14f18741b8789
Vikas <vck173@gmail.com>
2017-03-07 08:21:29 +0530

    ruby rack

---
+++ config.ru
@@ -0,0 +1,2 @@
+require File.expand_path('open_table', File.dirname(__FILE__))
+run OpenTable.new
---
+++ open_table.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'sinatra'
+require 'aws-sdk'
+require 'sinatra/base'
+
+class OpenTable  < Sinatra::Base
+
+	REGION = 'ap-southeast-1'
+	ACCESS_KEY = 'xxxxx'
+	ACCESS_SEC = 'xxxxx'
+	BUCKET = 'otimagestest'
+	BUCKET_FOLDER = 'images'
+	S3_URL = "https://s3-ap-southeast-1.amazonaws.com/" + BUCKET + "/"
+	REGEX = "images\/.+"
+	REGEX_THUMB = "thumbnails\/.+"
+	Aws.config.update({
+	  region: REGION,
+	  credentials: Aws::Credentials.new(ACCESS_KEY, ACCESS_SEC)
+	})
+
+	S3 = Aws::S3::Resource.new(region: REGION)
+
+	get '/' do
+	  erb :'upload'
+	end
+
+	post "/" do 
+		puts params
+
+		obj = S3.bucket(BUCKET).object(BUCKET_FOLDER + "/" + params['myImage'][:filename])
+		obj.upload_file(params['myImage'][:tempfile])
+		redirect "images"
+	end
+
+	get '/images' do 
+
+		imagesArr= (S3.bucket(BUCKET).objects.select{|x| !is_image?(x.key)})
+		imagesHash =  imagesArr.group_by{|x| x.last_modified.strftime("%m/%d/%Y")}
+		erb :images, :locals => {:images => imagesHash, :url => S3_URL }
+	end
+
+	get '/thumbnails' do 
+
+		imagesArr= (S3.bucket(BUCKET).objects.select{|x| !is_thumbnail?(x.key)})
+		imagesHash =  imagesArr.group_by{|x| x.last_modified.strftime("%m/%d/%Y")}
+		erb :thumbnails, :locals => {:images => imagesHash, :url => S3_URL }
+	end
+
+
+	def is_image?(key)
+		key.match(REGEX).nil?
+	end
+
+	def is_thumbnail?(key)
+		key.match(REGEX_THUMB).nil?
+	end
+end
